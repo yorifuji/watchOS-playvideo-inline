a5455a472be00a10bb5359b4306e744debe15871
yorifuji <yorifuji@gmail.com>
2020-05-24 16:13:14 +0900

    play video inline

--- watchOS-playvideo-inline WatchKit Extension/InterfaceController.swift
+++ watchOS-playvideo-inline WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,20 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var movie: WKInterfaceInlineMovie!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        movie.setPosterImage(WKImage(imageName: "poster"))
+
+        // auto play is true as default
+        movie.setAutoplays(false)
+
+        let path = Bundle.main.path(forResource: "sample", ofType: "mp4")!
+        let url = URL(fileURLWithPath: path)
+        movie.setMovieURL(url)
     }
     
     override func willActivate() {
@@ -26,6 +36,16 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+    @IBAction func onTapPlay() {
+        print(#function)
+        movie.play()
+    }
+
+    @IBAction func onTapPause() {
+        print(#function)
+        movie.pause()
     }
 
 }
